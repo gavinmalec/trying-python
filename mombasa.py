247fc15113117729ff7428185e68b24ee7d3284c
Gavin Maleche <60867459+gavinmalec@users.noreply.github.com>
2025-10-04 17:18:15 +0300

    Add files via upload

---
+++ mombasa.py
@@ -0,0 +1,25 @@
+print('mombasa trip on wenesday')
+x=5
+if x>1:
+ print('true')
+if x<1:
+ print('false')
+y=4
+while y>0:
+ print(y)
+ y=y-1
+print('blastoff')
+
+
+
+y=4
+while y>4:
+ print(y)
+ y=y-1
+ print('blastoff')
+        
+x=3
+if x>0:
+ print('true')
+if x<0:
+ print('false')
